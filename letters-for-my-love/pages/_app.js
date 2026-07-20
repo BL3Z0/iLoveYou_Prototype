0c2dbfed39aa6bb9e1d5cb92558b66d6a3413634
BL3Z0 <susookie4445@gmail.com>
2026-07-20 13:22:41 +0100

    Enhance MyApp with page prefetching on hover
    Added prefetching of pages on route change to improve navigation speed.

--- letters-for-my-love/pages/_app.js
+++ letters-for-my-love/pages/_app.js
@@ -1,10 +1,25 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  // Prefetch all pages on hover
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // Prefetch the next page
+      router.prefetch(url)
+    }
+    router.events.on('routeChangeStart', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router])
+
   return <Component {...pageProps} />
 }
 
-// Disable static generation for all pages
 MyApp.getInitialProps = async (ctx) => {
   return {
     pageProps: {}
